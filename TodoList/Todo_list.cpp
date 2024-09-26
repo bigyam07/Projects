cacc033a610e0e3ac8c0ec744cda1761ad37d360
b4u-UI <bigyamkarmacharya@gmail.com>
2024-09-26 09:33:55 +0545

    All Set

--- TodoList/Todo_list.cpp
+++ TodoList/Todo_list.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
-#include<fstream>
+#include<fstream> //For reading and writing from file
 #include<cstdio> //For rename and removing file
 using namespace std;
+
+void banner();
 class TodoList {
     public:
-        int count = 0;
         string task;
-        void banner();
         void addTask();
         void showTask();
         void searchTask();
@@ -14,7 +14,7 @@ class TodoList {
         void updateTask();
 };
 
-void TodoList :: banner() {
+void banner() {
     cout << "_________________________________" << endl;
     cout << "\t     My Todo" << endl;
     cout << "_________________________________" << endl;
@@ -47,6 +47,9 @@ void TodoList :: addTask() {
             return;
         }
     }
+    else {
+        system("cls");
+    }
 }
 
 void TodoList :: showTask() {
@@ -56,7 +59,9 @@ void TodoList :: showTask() {
     fin.open("todo.txt");
     banner();
     if(!fin.is_open()) {
-        cout << "Task is empty!!" << endl; 
+        system("cls");
+        cout << "Unable to read file!!" << endl; 
+        return;
     }
     bool flag = false;
     while(!fin.eof()) {
@@ -88,6 +93,7 @@ void TodoList :: searchTask() {
     cin.ignore();
     getline(cin, search);
     bool found = false;
+    int count = 0;
     while(!fin.eof()) {
         getline(fin, store,'\n');
         count ++;
@@ -129,7 +135,6 @@ void TodoList :: deleteTask() {
             fout << search << endl;
         }
         else {
-            count --;
             flag = true;
         }
     }
@@ -155,14 +160,56 @@ void TodoList :: deleteTask() {
     }
 }
 void TodoList :: updateTask() {
-    cout << "This is update";
+    system("cls");
+    banner();
+    string update, searchTask, searchFromFile;
+    cout << "which task you want to update? Please enter: ";
+    cin.ignore();
+    getline(cin, searchTask);
+    ifstream fin;
+    ofstream fout;
+    fin.open("todo.txt");
+    fout.open("temp.txt");
+    int flag = 0;
+    while(!fin.eof()) {
+        getline(fin, searchFromFile);
+        if(searchFromFile != searchTask) {
+            fout << searchFromFile << endl;
+        }
+        else {
+            cout << "Enter task to update: ";
+            getline(cin, update);
+            fout << update << endl;
+            flag = 1;
+        }
+    }
+    fin.close();
+    fout.close();
+    if(flag) {
+        remove("todo.txt");
+        rename("temp.txt", "todo.txt");
+        cout << "Updated Successful...!" << endl;
+    } else {
+        remove("temp.txt");
+        cout << "Task not found...!" << endl;
+    }
+    char exit;
+    cout << "Exit? (y/n): ";
+    cin >> exit;
+    if(exit !='y' && exit != 'Y') {
+        updateTask();
+    } else {
+        system("cls");
+        cout << "Exit Successful" << endl;
+    }
+
 }
 int main() {
     system("cls");
     TodoList list;
     bool option = true;
     while(option) {
-        list.banner();
+        banner();
         cout << "\n\t1. Add Task";
         cout << "\n\t2. Show Task";
         cout << "\n\t3. Search Task";
